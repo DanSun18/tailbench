c7044b85e44b1529fd37480f6b41734a90cad887
Dan Sun <dansun47@gmail.com>
2017-06-13 17:32:35 -0400

    fully compied version, now working on testing

--- moses/run_networked.sh
+++ moses/run_networked.sh
@@ -11,18 +11,18 @@ cp moses.ini.template moses.ini
 sed -i -e "s#@DATA_ROOT#$DATA_ROOT#g" moses.ini
 
 # Launch Server
-TBENCH_MAXREQS=15000 TBENCH_WARMUPREQS=1000 taskset -c $1 ${BINDIR}/moses_server_networked \
+TBENCH_MAXREQS=15000 TBENCH_WARMUPREQS=1000 taskset -c 0,1 ${BINDIR}/moses_server_networked \
     -config ./moses.ini \
     -input-file ${DATA_ROOT}/moses/testTerms \
     -threads ${THREADS} -num-tasks 100000 -verbose 0 &
 
 echo $! > server.pid
-$sudo chrt -f -p 99 $(cat server.pid)
+#$sudo chrt -f -p 99 $(cat server.pid)
 sleep 5
 
 # Launch Client
 
-TBENCH_QPS=300 TBENCH_MINSLEEPNS=10000 TBENCH_CLIENT_THREADS=4 taskset -c 2-9  ${BINDIR}/moses_client_networked &
+TBENCH_QPS=300 TBENCH_MINSLEEPNS=10000 TBENCH_CLIENT_THREADS=1 taskset -c 2,3  ${BINDIR}/moses_client_networked &
 echo $! > client.pid
 
 #sudo chrt -f -p 99 $(cat client.pid)
