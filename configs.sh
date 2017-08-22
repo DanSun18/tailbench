764e8dac43057cd57f233a8e9d2c7d412061835a
Dan Sun <dansun47@gmail.com>
2017-08-22 14:21:05 -0400

    fixed path

--- configs.sh
+++ configs.sh
@@ -1,6 +1,6 @@
 # Set this to point to the top level of the TailBench data directory
 # DATA_ROOT=/topleveldatadir
-DATA_ROOT=/scratch/yl408/tailbench.inputs
+DATA_ROOT=/home/ds318/tailbench.inputs
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
@@ -9,4 +9,4 @@ JDK_PATH=/data/sanchez/benchmarks/ubik-apps/jdk1.5.0_22
 
 # This location is used by applications to store scratch data during execution.
 # SCRATCH_DIR=/pathtoscratch
-SCRATCH_DIR=~/tailbench-v0.9/scratch
+SCRATCH_DIR=/home/ds318/tailbench/scratch
